24008b64166133811b05fa5cc8c6178c53f26315
Raghava Uppuluri <raghava.upp13@gmail.com>
2024-01-03 13:40:06 -0500

    fixed eval

--- rpal/scripts/eval_dataset.py
+++ rpal/scripts/eval_dataset.py
@@ -3,9 +3,9 @@ import numpy as np
 import argparse
 from rpal.utils.constants import *
 from rpal.utils.pcd_utils import scan2mesh, mesh2roi, visualize_pcds
+from rpal.algorithms.gui import HeatmapAnimation
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(
         description="Time-series Heatmap Generator and dataset evaluation"
     )
@@ -19,23 +19,28 @@ if __name__ == "__main__":
     dataset_path = RPAL_DATA_PATH / args.dataset_path
 
     # Data Load
-    # history = np.load(dataset_path / "search_history.npy")
-    # print(history.shape)
-    # ani = HeatmapAnimation(history, history[0][1])
-    # ani.visualize()
+    history = np.load(dataset_path / "search_history.npy")
+    print(history.shape)
+    ani = HeatmapAnimation(history)
+    ani.visualize()
 
     timeseries = np.load(dataset_path / "timeseries.npy")
-    print(timeseries.shape)
+
+    palpations_cnt = timeseries[:]["palp_id"].max()
+
+    print("Stats: ")
+    print(f"Palpations: {palpations_cnt}")
+
     reconstruction_pcd = o3d.io.read_point_cloud(
         str(dataset_path / "reconstruction.ply")
     )
 
     roi_pcd = o3d.io.read_point_cloud(str(dataset_path / "surface.ply"))
+    gt_scan = o3d.io.read_point_cloud(str(GT_PATH))
 
     roi_pcd.paint_uniform_color([0, 1, 1])
-    reconstruction_pcd.paint_uniform_color([1, 0, 0])
+    reconstruction_pcd.paint_uniform_color([0, 1, 0])
 
-    gt_scan = o3d.io.read_point_cloud(str(GT_PATH))
     visualize_pcds(
         [roi_pcd, reconstruction_pcd, gt_scan],
     )
